a57103591f97dfd560c2228848254822c0e2652d
Michael Souter <souterm1@gmail.com>
2023-11-07 11:56:40 -0500

    have started rendering text and almost nailed down position

--- heartrate.py
+++ heartrate.py
@@ -6,18 +6,39 @@ import time
 import mysql.connector
 import threading
 import random
+import math
 
 def heartrateVisual():
 # Initialize Pygame
     pygame.init()
 
     #Sets Display width and height in pixels
-    size = (800,400)
+    size = (1200,800)
     monitor_screen = pygame.display.set_mode(size, flags=pygame.NOFRAME)
     pygame.display.set_caption("Patient Heart Rate")
-
+    
+    #Font Ovelays
+    font = pygame.font.Font('freesansbold.ttf',50)
+    bpfont = pygame.font.Font('freesansbold.ttf',28)
+    red = (255,14,14,255)
+    black = (0,0,0,255)
+    green =(61,183,33,255)
+    blue = (0,0,255,255)
+       #BPM
+    hrtext = font.render('Pulse 60', True, green, black) 
+    hrtext_display = hrtext.get_rect()
+    hrtext_display.center = (size[0]*.9,size[1]*.33)
+        #Blood Pressure
+    bptext = bpfont.render('BP 120/80', True, red, black) 
+    bptext_display = bptext.get_rect()
+    bptext_display.center = (size[0]*.9,size[1]//2)
+        #O2Sat
+    oxtext = font.render('O2 90.0', True, blue, black) 
+    oxtext_display = oxtext.get_rect()
+    oxtext_display.center = (size[0]*.9,size[1]*.75)
     clock = pygame.time.Clock()
     alive = True
+
     # Drawing a nested display to get the fade out effect
     drawing = pygame.Surface(size, flags=pygame.SRCALPHA)
     drawing.fill((0,0,0,255))
@@ -28,8 +49,8 @@ def heartrateVisual():
     speed = 5
     #pointer
     ptr= -1
-    last_height = size[1] //2 
-
+    last_height = size[1] //3 
+    # OG : pulse_part = [0,1,0,0,6,0,-5,0,0,-1,0,0,2,0]
     #pulse tracker
     pulse_part = [0,1,0,0,6,0,-5,0,0,-1,0,0,2,0]
     pulse_position = -1
@@ -59,22 +80,27 @@ def heartrateVisual():
         if pulse_position >= 0:
             if pulse_position >= len(pulse_part):
                 pulse_position = -1
-                next_height = size[1] //2
+                next_height = size[1] //3
             else:
-                next_height = (size[1] //2 ) - (pulse_part[pulse_position] * vert_scale_factor)
+                next_height = (size[1] //3 ) - (pulse_part[pulse_position] * vert_scale_factor)
                 pulse_position +=1
         else:
             next_height = last_height
         
+
+
         drawing.blit(alpha_surf, (0,0), special_flags=pygame.BLEND_RGBA_MULT)
 
-        pygame.draw.line(drawing,(255,14,14,255),(ptr,last_height),(next_ptr,next_height),4)
+        pygame.draw.line(drawing,green,(ptr,last_height),(next_ptr,next_height),4)
         # Increments pointer to the end of the screen and resets the line to 0 pixels on x axis
         
         monitor_screen.blit(drawing,(0,0))
+        monitor_screen.blit(hrtext,hrtext_display)
+        monitor_screen.blit(bptext,bptext_display)
+        monitor_screen.blit(oxtext,oxtext_display)
         pygame.display.flip()
         
-        if next_ptr > size[0]:
+        if next_ptr > size[0]*0.8:
             ptr= 0
         else:
             ptr=next_ptr
@@ -120,19 +146,21 @@ def addToDB():
 
 
 
-db_thread =threading.Thread(target=addToDB)
-animation_thread = threading.Thread(target=heartrateVisual)
-beep_thread = threading.Thread(target=makebeep)
+heartrateVisual()
+
+#db_thread =threading.Thread(target=addToDB)
+#animation_thread = threading.Thread(target=heartrateVisual)
+#beep_thread = threading.Thread(target=makebeep)
 
 #Start Threads
-db_thread.start()
-animation_thread.start()
-beep_thread.start()
+#db_thread.start()
+#animation_thread.start()
+#beep_thread.start()
 
 #Join Threads
-db_thread.join()
-animation_thread.join()
-beep_thread.join()
+#db_thread.join()
+#animation_thread.join()
+#beep_thread.join()
 
 
 
